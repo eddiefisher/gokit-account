6e90a262c8806abb827198aec12b2ba48b0718b0
Eddie <eddie@aminoci.com>
2021-01-18 23:06:32 +0300

    fixed content type

--- account/middleware.go
+++ account/middleware.go
@@ -6,7 +6,7 @@ import (
 
 func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
 		next.ServeHTTP(w, r)
 	})
 }
